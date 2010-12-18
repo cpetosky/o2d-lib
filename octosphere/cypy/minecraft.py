71f90695e6e33d5170741090a47f798788bafb30
Cory Petosky <cory@petosky.net>
2010-12-18 14:39:26 -0800

    Changed two-space formatting to four-space formatting, to better fit with common Python formatting.
    Added an automatic backup feature to the Minecraft wrapper.

--- octosphere/cypy/minecraft.py
+++ octosphere/cypy/minecraft.py
@@ -22,208 +22,222 @@ from twisted.protocols import basic
 DEFAULT_PORT = 25564
 
 class Channel(basic.LineReceiver):
-  delimiter = '\n'
+    delimiter = '\n'
 
-  def connectionMade(self):
-    self.factory.channels.append(self)
-    self.authed = not bool(self.factory.secret)
-  
-  def connectionLost(self, reason):
-    self.factory.channels.remove(self)
+    def connectionMade(self):
+        self.factory.channels.append(self)
+        self.authed = not bool(self.factory.secret)
+    
+    def connectionLost(self, reason):
+        self.factory.channels.remove(self)
 
-  def lineReceived(self, line):
-    if self.authed:
-      self._handle_message(line)
-    else:
-      self._handle_auth(line)
+    def lineReceived(self, line):
+        if self.authed:
+            self._handle_message(line)
+        else:
+            self._handle_auth(line)
 
-  def _handle_message(self, message):
-    self.factory.server.send_command(message)
+    def _handle_message(self, message):
+        self.factory.server.send_command(message)
 
-  def _handle_auth(self, message):
-    if message != self.factory.secret:
-      self.transport.loseConnection()
-    else:
-      self.authed = True
+    def _handle_auth(self, message):
+        if message != self.factory.secret:
+            self.transport.loseConnection()
+        else:
+            self.authed = True
 
 
 class Server(object):
-  """A wrapper for the Minecraft Server."""
-  def __init__(self, port=DEFAULT_PORT, path='', secret=None):
-    self.port = port
-    self.factory = protocol.Factory()
-    self.factory.channels = []
-    self.factory.protocol = Channel
-    self.factory.secret = secret
-    self.factory.server = self
+    """A wrapper for the Minecraft Server."""
+    def __init__(self, port=DEFAULT_PORT, path='', secret=None):
+        self.port = port
+        self.factory = protocol.Factory()
+        self.factory.channels = []
+        self.factory.protocol = Channel
+        self.factory.secret = secret
+        self.factory.server = self
 
-    if path[-4:] != '.jar':
-      path = os.path.join(path, 'minecraft_server.jar')
-    self.path = path
+        if path[-4:] != '.jar':
+            path = os.path.join(path, 'minecraft_server.jar')
+        self.path = path
 
-  def start(self):    
-    # Start up the Minecraft server
-    cmd = 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
-    self.process = Popen(
-        cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    thread.start_new(self._process_output, ())
-    reactor.listenTCP(self.port, self.factory)
-    reactor.run()
+    def start(self):        
+        # Start up the Minecraft server
+        cmd = 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
+        self.process = Popen(
+                cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        thread.start_new(self._process_output, ())
+        reactor.listenTCP(self.port, self.factory)
+        reactor.run()
 
-  def _process_output(self):
-    """Processes the Minecraft server's output."""
-    while True:
-      line = self.process.stdout.readline()[:-1]
-      for channel in self.factory.channels:
-        channel.sendLine(str(line))
-        channel.transport.doWrite()
+    def _process_output(self):
+        """Processes the Minecraft server's output."""
+        while True:
+            line = self.process.stdout.readline()[:-1]
+            for channel in self.factory.channels:
+                channel.sendLine(str(line))
+                channel.transport.doWrite()
 
-  def send_command(self, command):
-    if command[-1] != '\n':
-      command += '\n'
-    self.process.stdin.write(command)
+    def send_command(self, command):
+        if command[-1] != '\n':
+            command += '\n'
+        self.process.stdin.write(command)
 
 class ClientChannel(basic.LineReceiver):
-  delimiter = '\n'
+    delimiter = '\n'
 
-  __logged_in_regex = re.compile(
-      r'^(?P<user>[A-Za-z0-9_-]+) \[.*\] logged in with entity id [0-9]+$')
-  __logged_out_regex = re.compile(
-      r'^(?P<user>[A-Za-z0-9_-]+) lost connection:')
-  __list_regex = re.compile(
-      r'^Connected players: (?P<users>([A-Za-z0-9_-]+(, )?)*)$')
-  __command_regex = re.compile(
-      r'^(?P<user>[A-Za-z0-9_-]+) tried command: (?P<command>[A-Za-z0-9_ -]+)$')
-  
-  def connectionMade(self):
-    self.client = self.factory.client
-    if self.client.secret:
-      self.send_command(self.client.secret)
-    self.send_command('list')
-    self.client.protocol = self
-    self.client.on_ready()
+    __parsable_lines = {
+        'logged_in': re.compile(
+                r'^(?P<user>[A-Za-z0-9_-]+) \[.*\] '
+                r'logged in with entity id [0-9]+$'),
+        'logged_out': re.compile(
+                r'^(?P<user>[A-Za-z0-9_-]+) lost connection:'),
+        'list': re.compile(
+                r'^Connected players: (?P<users>([A-Za-z0-9_-]+(, )?)*)$'),
+        'command': re.compile(
+                r'^(?P<user>[A-Za-z0-9_-]+) tried command: '
+                r'(?P<command>[A-Za-z0-9_ -]+)$'),
+    }
 
-  def send_command(self, command):
-    if len(command) == 0:
-      return
-    self.sendLine(str(command))
-    self.transport.doWrite()
+    def connectionMade(self):
+        self.client = self.factory.client
+        if self.client.secret:
+            self.send_command(self.client.secret)
+        self.send_command('list')
+        self.client.protocol = self
+        self.client.on_ready()
 
-  def lineReceived(self, line):
-    sentinel = '[INFO] '
-    if sentinel in line:
-      line = line[line.find(sentinel) + len(sentinel):]
-    print line
+    def send_command(self, command):
+        if len(command) == 0:
+            return
+        self.sendLine(str(command))
+        self.transport.doWrite()
 
-    # Handle logged in
-    match = self.__logged_in_regex.match(line)
-    if match:
-      user = match.group('user')
-      if len(self.client.users) == 0:
-        other_users = "None"
-      else:
-        other_users = ', '.join(self.client.users)
-      self.client.users.add(user)
-      self.client.on_logged_in(user)
-      return
-    
-    # Handle logged out
-    match = self.__logged_out_regex.match(line)
-    if match:
-      user = match.group('user')
-      self.client.users.discard(user)
-      self.client.on_logged_out(user)
-      return
+    def lineReceived(self, line):
+        sentinel = '[INFO] '
+        if sentinel in line:
+            line = line[line.find(sentinel) + len(sentinel):]
+        print line
 
-    # Handle list
-    match = self.__list_regex.match(line)
-    if match:
-      users = match.group('users')
-      users = users.split(',')
-      self.client.users = set(user.strip() for user in users)
-      return
+        for processor, regex in self.__parsable_lines.iteritems():
+            match = regex.match(line)
+            if match:
+                func = getattr(self, 'on_message_%s' % processor, None)
+                if func:
+                    func(match)
+                else:
+                    print 'Warning: no processing function for %s' % processor
+                break
+        # Doesn't match anything we understand, queue it for other processors
+        self.client.on_unknown_line(line)
+        self.client.output.put(line)
 
-    # Handle command
-    match = self.__command_regex.match(line)
-    if match:
-      user = match.group('user')
-      command = match.group('command')
-      command = command.split()
-      command, args = command[0], command[1:]
-      self.client.on_user_command(user, command, args)
-      return
-    
-    # Doesn't match anything we understand, queue it for other processors
-    self.client.on_unknown_line(line)
-    self.client.output.put(line)
+    def on_message_logged_in(self, match):
+        user = match.group('user')
+        if len(self.client.users) == 0:
+            other_users = "None"
+        else:
+            other_users = ', '.join(self.client.users)
+        self.client.users.add(user)
+        self.client.on_logged_in(user)
+
+    def on_message_logged_out(self, match):        
+        user = match.group('user')
+        self.client.users.discard(user)
+        self.client.on_logged_out(user)
+
+    def on_message_list(self, match):
+        users = match.group('users')
+        users = users.split(',')
+        self.client.users = set(user.strip() for user in users)
+
+    def on_message_command(self, match):
+        user = match.group('user')
+        command = match.group('command')
+        command = command.split()
+        command, args = command[0], command[1:]
+        self.client.on_user_command(user, command, args)
 
 
 class AdminClient(object):
-  """An admin console that can connect to a cypy.minecraft.Server instance."""
+    """An admin console that can connect to a cypy.minecraft.Server instance."""
 
-  def connect(self, secret=None, host=None, port=None):
-    if not host:
-      host = 'localhost'
-    if not port:
-      port = DEFAULT_PORT
-    self.secret = secret
-    self.output = Queue()
-    self.users = set()
-    factory = protocol.ClientFactory()
-    factory.protocol = ClientChannel
-    factory.client = self
-    reactor.connectTCP(host, port, factory)
-    reactor.run()
+    def connect(self, secret=None, host=None, port=None):
+        if not host:
+            host = 'localhost'
+        if not port:
+            port = DEFAULT_PORT
+        self.secret = secret
+        self.output = Queue()
+        self.users = set()
+        factory = protocol.ClientFactory()
+        factory.protocol = ClientChannel
+        factory.client = self
+        reactor.connectTCP(host, port, factory)
+        reactor.run()
 
-  def send_command(self, command):
-    if self.protocol:
-      self.protocol.send_command(command)
+    def send_command(self, command):
+        if self.protocol:
+            self.protocol.send_command(command)
 
-  def get_output_line(self):
-    try:
-      return self.output.get(True, 2)
-    except Empty:
-      return None
+    def get_output_line(self):
+        try:
+            return self.output.get(True, 2)
+        except Empty:
+            return None
 
-  def has_output(self):
-    return self.output.empty()
+    def has_output(self):
+        return self.output.empty()
 
-  def clear_output(self):
-    try:
-      while True:
-        self.output.get_nowait()
-    except Empty:
-      pass
+    def clear_output(self):
+        try:
+            while True:
+                self.output.get_nowait()
+        except Empty:
+            pass
 
-  def on_ready(self):
-    pass
+    def on_ready(self):
+        pass
 
-  def on_logged_in(self, user):
-    pass
+    def on_logged_in(self, user):
+        pass
 
-  def on_logged_out(self, user):
-    pass
+    def on_logged_out(self, user):
+        pass
 
-  def on_user_command(self, user, command, args):
-    pass
-  
-  def on_unknown_line(self, line):
-    pass
+    def on_user_command(self, user, command, args):
+        """Called when a user sends a command not implemented by stock server.
+
+        On the command `/foo`, the client wrapper attempts to call
+        `self.on_user_command_foo`. If it doesn't exist, it instead calls
+        `self.on_unknown_user_command`.
+        """
+        func = getattr(self, 'on_user_command_%s' % command, None)
+        if func:
+            func(user, args)
+        else:
+            self.on_unknown_user_command(user, command, args)
+
+    def on_unknown_user_command(self, user, command, args):
+        pass
+    
+    def on_unknown_line(self, line):
+        pass
 
 
 if __name__ == '__main__':
-  parser = optparse.OptionParser()
-  parser.add_option(
-      '-m', '--mode', default='server',
-      help='one of "client","server" [default=server]')
-  parser.add_option(
-      '-s', '--secret',
-      help='shared secret between servers and clients')
-  parser.add_option(
-      '-p', '--port', type='int', default=DEFAULT_PORT,
-      help='port to listen/connect on')
-  options, args = parser.parse_args()
-  if options.mode == 'server':
-    server = Server(port=options.port, secret=options.secret)
-    server.start()
+    parser = optparse.OptionParser()
+    parser.add_option(
+            '-m', '--mode', default='server',
+            help='one of "client","server" [default=server]')
+    parser.add_option(
+            '-s', '--secret',
+            help='shared secret between servers and clients')
+    parser.add_option(
+            '-p', '--port', type='int', default=DEFAULT_PORT,
+            help='port to listen/connect on')
+    options, args = parser.parse_args()
+    if options.mode == 'server':
+        server = Server(port=options.port, secret=options.secret)
+        server.start()
 
--- octosphere/octocraft/irc.py
+++ octosphere/octocraft/irc.py
@@ -3,57 +3,59 @@ import octosphere
 import sys
 
 class MinecraftProcessor(octosphere.CommandProcessor):
-  def __init__(self, secret):
-    octosphere.CommandProcessor.__init__(self)
-    self.secret = secret
-    self.ops = []
-    self._prefix = 'minecraft'
+    def __init__(self, secret):
+        octosphere.CommandProcessor.__init__(self)
+        self.secret = secret
+        self.ops = []
+        self._prefix = 'minecraft'
 
-  def handle_list(self, bot, sender, message):
-    users = ', '.join(bot.server.users) if bot.server.users else 'None'
-    bot.send_message(sender, 'Connected players: %s' % users)
-    return True
+    def handle_list(self, bot, sender, message):
+        users = ', '.join(bot.server.users) if bot.server.users else 'None'
+        bot.send_message(sender, 'Connected players: %s' % users)
+        return True
 
-  def handle_raw(self, bot, sender, message):
-    if sender in self.ops:
-      bot.server.send_command(message)
-    else:
-      bot.send_message(sender, 'Must auth first.')
+    def handle_raw(self, bot, sender, message):
+        if sender in self.ops:
+            bot.server.send_command(message)
+        else:
+            bot.send_message(sender, 'Must auth first.')
 
-  def handle_auth(self, bot, sender, message):
-    if sender in self.ops:
-      bot.send_message(sender, 'You are already an op.')
-      return
+    def handle_auth(self, bot, sender, message):
+        if sender in self.ops:
+            bot.send_message(sender, 'You are already an op.')
+            return
 
-    tokens = message.split()
-    if len(tokens) != 1:
-      bot.send_message(sender, 'Try AUTH <secret>')
-      return
+        tokens = message.split()
+        if len(tokens) != 1:
+            bot.send_message(sender, 'Try AUTH <secret>')
+            return
 
-    secret = tokens[0]
-    if secret != self.secret:
-      bot.send_message(sender, 'Incorrect secret.')
-      return
+        secret = tokens[0]
+        if secret != self.secret:
+            bot.send_message(sender, 'Incorrect secret.')
+            return
 
-    self.ops.append(sender)
-    bot.send_message(sender, 'Authed successfully.')
-    return True
+        self.ops.append(sender)
+        bot.send_message(sender, 'Authed successfully.')
+        return True
 
 
 if __name__ == '__main__':
-  args = sys.argv[1:]
-  secret = args[0]
-  if len(args) > 1:
-    host = args[1]
-  else:
-    host = 'localhost'
-  if len(args) > 2:
-    irchost = args[2]
-  else:
-    irchost = 'irc.synirc.net'
-  client = minecraft.AdminClient()
-  client.connect(secret, host=host)
-  bot = octosphere.Octosphere()
-  bot.processors += [octosphere.DefaultProcessor(), MinecraftProcessor(secret)]
-  bot.server = client
-  bot.start(irchost)
+    args = sys.argv[1:]
+    secret = args[0]
+    if len(args) > 1:
+        host = args[1]
+    else:
+        host = 'localhost'
+    if len(args) > 2:
+        irchost = args[2]
+    else:
+        irchost = 'irc.synirc.net'
+    client = minecraft.AdminClient()
+    client.connect(secret, host=host)
+    bot = octosphere.Octosphere()
+    bot.processors += [
+            octosphere.DefaultProcessor(),
+            MinecraftProcessor(secret)]
+    bot.server = client
+    bot.start(irchost)
